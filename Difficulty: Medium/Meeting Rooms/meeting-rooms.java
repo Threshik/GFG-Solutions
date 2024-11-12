6ded7b76706e6d293be007860dcdd85dc4016aee
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-11-12 22:00:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.canAttend(arr);
+            if (ans)
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // Your code here
+        int n = arr.length;
+
+        // Sort the meetings by their start times
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 0; i < n - 1; i++) {
+
+            // Compare the current meeting's end time with the
+            // next meeting's start time to check for overlap
+            if (arr[i][1] > arr[i + 1][0]) return false;
+        }
+        return true;
+    }
+}
