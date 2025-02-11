8a3fa25ca3f746b249da561a63e30d605810491a
Threshika V S <115970544+Threshik@users.noreply.github.com>
2025-02-11 18:14:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.java
+++ Difficulty: Easy/Check for BST/check-for-bst.java
@@ -98,7 +98,9 @@ class GfG {
             else
                 System.out.println("false");
             t--;
-        }
+        
+System.out.println("~");
+}
     }
 }
 
@@ -109,35 +111,19 @@ class GfG {
 
 class Solution {
     // Function to check whether a Binary Tree is BST or not.
-    int maxval(Node root)
-    {
-        if(root==null)
-        return Integer.MIN_VALUE;
-        int val = root.data;
-        int leftmax= maxval(root.left);
-        int rightmax=maxval(root.right);
-        return Math.max(val,Math.max(leftmax,rightmax));
+ boolean isBST(Node root) {
+    return helper(root,Integer.MAX_VALUE,Integer.MIN_VALUE);
     }
-    int minval(Node root)
-    {
-        if(root==null)
-        return Integer.MAX_VALUE;
-        int val=root.data;
-        int leftmin=minval(root.left);
-        int rightmin=minval(root.right);
-        return Math.min(val,Math.min(leftmin,rightmin));
-    }
-    boolean isBST(Node root) {
-        // code here.
-        if(root==null)
-        return true;
-        if(root.left!=null && maxval(root.left)>=root.data)
-        return false;
-        if(root.right!=null && minval(root.right)<=root.data)
-        return false;
-        if(isBST(root.left)==false || isBST(root.right)==false)
-        return false;
-        return true;
-        
+    Boolean helper(Node root,int max,int min){
+        if(root==null){
+            return true;
+        }
+         
+         if(root.data<min||root.data>max){
+             return false;
+         }
+         
+        return helper(root.left,root.data,min)&&helper(root.right,max,root.data); 
+
     }
 }
